0d053f6c355948b3e210445036e28d7c27e20495
tushar-1910 <tusharagg19@gmail.com>
2022-04-17 17:25:41 +0530

    address page and media query added

--- js/index.js
+++ js/index.js
@@ -14,216 +14,216 @@ function slider() {
   x[index-1].style.display = "block";  
   setTimeout(slider, 2000); 
 }
-var bestSellerData= [
+// var bestSellerData= [
   
-]
+// ]
 
-function displayData(bestSellerData) {
-  document.querySelector('#bestSellers').innerHTML = '';
-  bestSellerData.forEach(ele => {
-    var box = document.createElement('div');
-    box.setAttribute('class', 'box')
-    box.style.borderRadius="20px";
+// function displayData(bestSellerData) {
+//   document.querySelector('#bestSellers').innerHTML = '';
+//   bestSellerData.forEach(ele => {
+//     var box = document.createElement('div');
+//     box.setAttribute('class', 'box')
+//     box.style.borderRadius="20px";
 
-   var imgDiv =document.createElement('div');
-    var fimg = document.createElement('img');
-    fimg.src = ele.foodImg;
-    fimg.style.width="100%";
-    fimg.style.height="230px";
-    fimg.style.borderTopLeftRadius="20px";
-    fimg.style.borderTopRightRadius="20px";
-    imgDiv.append(fimg);
+//    var imgDiv =document.createElement('div');
+//     var fimg = document.createElement('img');
+//     fimg.src = ele.foodImg;
+//     fimg.style.width="100%";
+//     fimg.style.height="230px";
+//     fimg.style.borderTopLeftRadius="20px";
+//     fimg.style.borderTopRightRadius="20px";
+//     imgDiv.append(fimg);
 
-   var innerBox=document.createElement("div");
+//    var innerBox=document.createElement("div");
 
-    var fname = document.createElement('h3');
-    fname.textContent = ele.foodName;
-    fname.style.fontFamily="Arial, Helvetica, sans-serif";
-    fname.style.padding="10px";
-    fname.style.marginTop="-10px";
-    fname.style.marginBottom="-10px";
+//     var fname = document.createElement('h3');
+//     fname.textContent = ele.foodName;
+//     fname.style.fontFamily="Arial, Helvetica, sans-serif";
+//     fname.style.padding="10px";
+//     fname.style.marginTop="-10px";
+//     fname.style.marginBottom="-10px";
      
-    var fDec=document.createElement('p');
-    fDec.textContent=ele.foodDescription;
+//     var fDec=document.createElement('p');
+//     fDec.textContent=ele.foodDescription;
     
-    var wetBox=document.createElement("div");
-    wetBox.style.display="flex";
-    wetBox.style.justifyContent='center';
-    wetBox.style.verticalAlign="center";
+//     var wetBox=document.createElement("div");
+//     wetBox.style.display="flex";
+//     wetBox.style.justifyContent='center';
+//     wetBox.style.verticalAlign="center";
 
-    var newt=document.createElement("h4");
-    newt.textContent=ele.netWeight;
-    newt.style.fontSize='20px';
+//     var newt=document.createElement("h4");
+//     newt.textContent=ele.netWeight;
+//     newt.style.fontSize='20px';
 
-    var wtim=document.createElement("img");
-    wtim.src=ele.weightImg;
-   wtim.style.height='20px';
+//     var wtim=document.createElement("img");
+//     wtim.src=ele.weightImg;
+//    wtim.style.height='20px';
 
-  //  justify-content: center;
-  //  align-items: center;
+//   //  justify-content: center;
+//   //  align-items: center;
 
-    var gwt=document.createElement("h4");
-    gwt.textContent=ele.grossWeight;
-    gwt.style.fontSize='20px';
+//     var gwt=document.createElement("h4");
+//     gwt.textContent=ele.grossWeight;
+//     gwt.style.fontSize='20px';
 
-    wetBox.append(newt, wtim, gwt);
+//     wetBox.append(newt, wtim, gwt);
 
-    var priceBox=document.createElement("div");
-    priceBox.style.display="flex";
-    priceBox.style.width="95%";
-    priceBox.style.margin="auto";
-    priceBox.style.justifyContent="space-between";
+//     var priceBox=document.createElement("div");
+//     priceBox.style.display="flex";
+//     priceBox.style.width="95%";
+//     priceBox.style.margin="auto";
+//     priceBox.style.justifyContent="space-between";
   
-    var pri=document.createElement("h2");
-    pri.textContent=ele.prcie;
+//     var pri=document.createElement("h2");
+//     pri.textContent=ele.prcie;
 
-    var cartBut=document.createElement("button");
-    cartBut.setAttribute("id", "addToCart");
-    cartBut.textContent="Add to cart";
-    cartBut.style.backgroundColor="rgb(230, 46, 0)";
-    cartBut.style.color="white";
-    cartBut.style.verticalAlign="";
-    cartBut.style.fontFamily="arial";
-    cartBut.style.borderRadius="10px";
-    cartBut.style.fontSize="18px";
-    cartBut.style.height="40px";
-    cartBut.style.border="none";
+//     var cartBut=document.createElement("button");
+//     cartBut.setAttribute("id", "addToCart");
+//     cartBut.textContent="Add to cart";
+//     cartBut.style.backgroundColor="rgb(230, 46, 0)";
+//     cartBut.style.color="white";
+//     cartBut.style.verticalAlign="";
+//     cartBut.style.fontFamily="arial";
+//     cartBut.style.borderRadius="10px";
+//     cartBut.style.fontSize="18px";
+//     cartBut.style.height="40px";
+//     cartBut.style.border="none";
 
   
-    cartBut.addEventListener('click', function() {
-      cartData.push(ele);
-        alert("item added successfuly to cart");
-        localStorage.setItem('cartData', JSON.stringify(cartData));
-      });
+//     cartBut.addEventListener('click', function() {
+//       cartData.push(ele);
+//         alert("item added successfuly to cart");
+//         localStorage.setItem('cartData', JSON.stringify(cartData));
+//       });
 
-      priceBox.append(pri, cartBut);
+//       priceBox.append(pri, cartBut);
 
-      innerBox.append(fname, fDec, wetBox, priceBox);
+//       innerBox.append(fname, fDec, wetBox, priceBox);
 
-      var deliTime=document.createElement('div');
-       deliTime.style.display="flex";
-       deliTime.style.borderTop="2px rgb(230, 230, 230)";
-       deliTime.style.justifyContent="center";
+//       var deliTime=document.createElement('div');
+//        deliTime.style.display="flex";
+//        deliTime.style.borderTop="2px rgb(230, 230, 230)";
+//        deliTime.style.justifyContent="center";
 
 
-       var delim=document.createElement("img");
-       delim.src=ele.deliveryTimeImg;
+//        var delim=document.createElement("img");
+//        delim.src=ele.deliveryTimeImg;
 
-       var tm=document.createElement("h3");
-       tm.textContent=ele.deliveryTime;
-       tm.style.color="gray";
-       tm.style.fontSize="16px";
+//        var tm=document.createElement("h3");
+//        tm.textContent=ele.deliveryTime;
+//        tm.style.color="gray";
+//        tm.style.fontSize="16px";
 
-       deliTime.append(delim, tm);
+//        deliTime.append(delim, tm);
 
-       box.append(imgDiv, innerBox, deliTime);
-       document.querySelector("#bestSellers").append(box);
-  });
-}
-displayData(bestSellerData);
+//        box.append(imgDiv, innerBox, deliTime);
+//        document.querySelector("#bestSellers").append(box);
+//   });
+// }
+// displayData(bestSellerData);
 
 //new div
 
-var boneLessCutsData= [
+// var boneLessCutsData= [
   
-]
+// ]
 
-function displayData(boneLessCutsData) {
-  document.querySelector('#bonelessCuts').innerHTML = '';
-  boneLessCutsData.forEach(ele => {
-    var box = document.createElement('div');
-    box.setAttribute('class', 'box')
-    box.style.borderRadius="20px";
+// function displayData(boneLessCutsData) {
+//   document.querySelector('#bonelessCuts').innerHTML = '';
+//   boneLessCutsData.forEach(ele => {
+//     var box = document.createElement('div');
+//     box.setAttribute('class', 'box')
+//     box.style.borderRadius="20px";
 
-   var imgDiv =document.createElement('div');
-    var fimg = document.createElement('img');
-    fimg.src = ele.foodImg;
-    fimg.style.width="100%";
-    fimg.style.height="230px";
-    fimg.style.borderTopLeftRadius="20px";
-    fimg.style.borderTopRightRadius="20px";
-    imgDiv.append(fimg);
+//    var imgDiv =document.createElement('div');
+//     var fimg = document.createElement('img');
+//     fimg.src = ele.foodImg;
+//     fimg.style.width="100%";
+//     fimg.style.height="230px";
+//     fimg.style.borderTopLeftRadius="20px";
+//     fimg.style.borderTopRightRadius="20px";
+//     imgDiv.append(fimg);
 
-   var innerBox=document.createElement("div");
+//    var innerBox=document.createElement("div");
 
-    var fname = document.createElement('h3');
-    fname.textContent = ele.foodName;
-    fname.style.fontFamily="Arial, Helvetica, sans-serif";
-    fname.style.padding="10px";
-    fname.style.marginTop="-10px";
-    fname.style.marginBottom="-10px";
+//     var fname = document.createElement('h3');
+//     fname.textContent = ele.foodName;
+//     fname.style.fontFamily="Arial, Helvetica, sans-serif";
+//     fname.style.padding="10px";
+//     fname.style.marginTop="-10px";
+//     fname.style.marginBottom="-10px";
      
-    var fDec=document.createElement('p');
-    fDec.textContent=ele.foodDescription;
+//     var fDec=document.createElement('p');
+//     fDec.textContent=ele.foodDescription;
 
-    var wetBox=document.createElement("div");
-    wetBox.style.display="flex";
-    wetBox.style.justifyContent='center';
-    wetBox.style.verticalAlign="center";
+//     var wetBox=document.createElement("div");
+//     wetBox.style.display="flex";
+//     wetBox.style.justifyContent='center';
+//     wetBox.style.verticalAlign="center";
 
-    var newt=document.createElement("h4");
-    newt.textContent=ele.netWeight;
-    newt.style.fontSize='20px';
+//     var newt=document.createElement("h4");
+//     newt.textContent=ele.netWeight;
+//     newt.style.fontSize='20px';
 
-    var wtim=document.createElement("img");
-    wtim.src=ele.weightImg;
-   wtim.style.height='20px';
+//     var wtim=document.createElement("img");
+//     wtim.src=ele.weightImg;
+//    wtim.style.height='20px';
 
-    var gwt=document.createElement("h4");
-    gwt.textContent=ele.grossWeight;
-    gwt.style.fontSize='20px';
+//     var gwt=document.createElement("h4");
+//     gwt.textContent=ele.grossWeight;
+//     gwt.style.fontSize='20px';
 
-    wetBox.append(newt, wtim, gwt);
+//     wetBox.append(newt, wtim, gwt);
 
-    var priceBox=document.createElement("div");
-    priceBox.style.display="flex";
-    priceBox.style.width="95%";
-    priceBox.style.margin="auto";
-    priceBox.style.justifyContent="space-between";
+//     var priceBox=document.createElement("div");
+//     priceBox.style.display="flex";
+//     priceBox.style.width="95%";
+//     priceBox.style.margin="auto";
+//     priceBox.style.justifyContent="space-between";
   
-    var pri=document.createElement("h2");
-    pri.textContent=ele.prcie;
+//     var pri=document.createElement("h2");
+//     pri.textContent=ele.prcie;
 
-    var cartBut=document.createElement("button");
-    cartBut.textContent="Add to cart";
-    cartBut.style.backgroundColor="rgb(230, 46, 0)";
-    cartBut.style.color="white";
-    cartBut.style.verticalAlign="";
-    cartBut.style.fontFamily="arial";
-    cartBut.style.borderRadius="10px";
-    cartBut.style.fontSize="18px";
-    cartBut.style.height="40px";
-    cartBut.style.border="none";
+//     var cartBut=document.createElement("button");
+//     cartBut.textContent="Add to cart";
+//     cartBut.style.backgroundColor="rgb(230, 46, 0)";
+//     cartBut.style.color="white";
+//     cartBut.style.verticalAlign="";
+//     cartBut.style.fontFamily="arial";
+//     cartBut.style.borderRadius="10px";
+//     cartBut.style.fontSize="18px";
+//     cartBut.style.height="40px";
+//     cartBut.style.border="none";
 
   
-    cartBut.addEventListener('click', function() {
-      cartData.push(ele);
-        alert("item added successfuly to cart");
-        localStorage.setItem('cartData', JSON.stringify(ele));
-      });
+//     cartBut.addEventListener('click', function() {
+//       cartData.push(ele);
+//         alert("item added successfuly to cart");
+//         localStorage.setItem('cartData', JSON.stringify(ele));
+//       });
 
-      priceBox.append(pri, cartBut);
+//       priceBox.append(pri, cartBut);
 
-      innerBox.append(fname, fDec, wetBox, priceBox);
+//       innerBox.append(fname, fDec, wetBox, priceBox);
 
-      var deliTime=document.createElement('div');
-       deliTime.style.display="flex";
-       deliTime.style.borderTop="2px rgb(230, 230, 230)";
-       deliTime.style.justifyContent="center";
+//       var deliTime=document.createElement('div');
+//        deliTime.style.display="flex";
+//        deliTime.style.borderTop="2px rgb(230, 230, 230)";
+//        deliTime.style.justifyContent="center";
 
 
-       var delim=document.createElement("img");
-       delim.src=ele.deliveryTimeImg;
+//        var delim=document.createElement("img");
+//        delim.src=ele.deliveryTimeImg;
 
-       var tm=document.createElement("h3");
-       tm.textContent=ele.deliveryTime;
-       tm.style.color="gray";
-       tm.style.fontSize="16px";
+//        var tm=document.createElement("h3");
+//        tm.textContent=ele.deliveryTime;
+//        tm.style.color="gray";
+//        tm.style.fontSize="16px";
 
-       deliTime.append(delim, tm);
+//        deliTime.append(delim, tm);
 
-       box.append(imgDiv, innerBox, deliTime);
-       document.querySelector("#bestSellers").append(box);
-  });
-}
-displayData(boneLessCutsData);
+//        box.append(imgDiv, innerBox, deliTime);
+//        document.querySelector("#bestSellers").append(box);
+//   });
+// }
+// displayData(boneLessCutsData);
